42397a8b2ce26665f4982ce51c59910c4d3dd99d
DJ Bonner <djbonner@mtu.edu>
2014-02-28 15:36:44 -0500

    quickServer with short readme and install script

---
+++ qServer/handleServer.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+BINPATH=/usr/local/bin
+
+if [[ "$1" == "install" ]]; then
+	chmod a+x ./quickServer
+
+	if [[ ! -e /usr/local/bin/quickServer ]]; then
+		sudo cp quickServer $BINPATH
+		echo "copied ./quickServer to $BINPATH/quickServer"
+	else
+		echo "quickServer already in $BINPATH"
+	fi
+
+elif [[ "$1" == "uninstall" ]]; then
+	if [[ -e /usr/local/bin/quickServer ]]; then
+		sudo rm /usr/local/bin/quickServer
+		echo "removed $BINPATH/quickServer"
+	else
+		echo "quickServer not in $BINPATH"
+	fi
+else
+	echo 'type ./installServer COMMAND'
+	echo 'Command options - install or uninstall'
+fi
