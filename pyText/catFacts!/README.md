f02e5e67134c3eef95e0228b2a031b470f7d9a78
The Don <donsbons@gmail.com>
2014-04-11 10:12:20 -0400

    first commit

---
+++ pyText/catFacts!/README.md
@@ -0,0 +1,8 @@
+pyText
+======
+
+This is a script for the common prank of CatFacts!  The idea is to run the script to annoy your friends.  There will be a file input with a list of all the different cat facts!  Every three cat facts there will be a prompt to quit the service.  The code to quit the service will of course be a random string that is impossible to repeat.  
+
+On a real note, this will check a mail account to respond accordingly to a person trying to cancel the CatFacts!  The text service in the parent folder will also be taken advantage of.  Hopefully your friends will love it!
+
+***Python 3***
---
+++ pyText/catFacts!/catFacts.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+# DJ Bonner
+# Script to send SMS text via gmail server using python 3
+
+import smtplib, sys, getpass
+
+# Check for python3
+if sys.hexversion < 0x030000F0:
+    print("This script requires Python 3")
+    sys.exit(1)
+
+# Prompt for phone number
+print('What is the receiving phone number?')
+number = sys.stdin.readline().strip().replace('-','')
+if len(number) != 10:
+	print('Invalid phone number')
+	sys.exit(1)
+
+#Make sure valid provider
+print('What is the receiving phone provider? (at&t, verizon, or tmobile)')
+while True:
+	prov = sys.stdin.readline().strip().lower()
+	if prov == ('at&t' or 'att'):
+		host = 'txt.att.net'
+		break
+	elif prov == 'verizon':
+		host = 'vtext.com'
+		break
+	elif prov == ('tmobile' or 't-mobile'):
+		host = 'tmomail.net'
+		break
+	else:
+		print('Not a valid provider.  Re enter provider. (at&t, verizon, or tmobile)')
+
+# Format message to send
+# Different host names of providers:
+# 	vtext.com - verizon
+# 	tmomail.net - tmobile
+# 	mms.att.net - at&t
+textTo = number + '@' + host
+
+# Input message
+print('Type a message!')
+message = sys.stdin.readline().strip()
+
+# Setup gmail server
+server = smtplib.SMTP('smtp.gmail.com',587)
+server.starttls()
+
+# Enter gmail information
+print('Enter your gmail acount')
+username = sys.stdin.readline().strip()
+password = getpass.getpass("Password for " + username + ":")
+server.login(username, password)
+
+# Send the message
+server.sendmail(username, textTo, message)
+print('Message sent!')
+
+# Logout the server
+server.quit()
