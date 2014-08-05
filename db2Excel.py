7d9b84714ca7359248223874bb3861813503fbd0
The Don <donsbons@gmail.com>
2014-08-05 10:15:37 -0400

    WHOOPS.  Im a dumbass

--- db2Excel.py
+++ db2Excel.py
@@ -2,17 +2,18 @@
 
 import sqlite3
 
-host     = raw_input('host:')
-user     = raw_input('user:')
-passwd   = raw_input('passwd:')
-dbName   = raw_input('dbName:')
+host     = raw_input( 'host:' )
+user     = raw_input( 'user:' )
+passwd   = raw_input( 'passwd:' )
+dbName   = raw_input( 'dbName:' )
 output   = dbName + '.xls'
+table    = taw_input( 'table:' )
 
 print( '%s database will be saved to %s.' % ( dbName, output ) )
 
 conn = pymssql.connect( host = host, user = user, password = passwd, database = dbName )
 cur = conn.cursor()
-cur.execute( 'SELECT * FROM wifi' )
+cur.execute( 'SELECT * FROM %s' % table )
 table = cur.fetchall()
 for row in table:
 	print row
